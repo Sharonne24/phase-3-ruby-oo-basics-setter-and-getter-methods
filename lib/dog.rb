937c014184fe0539e24163cb254afa07d5500048
Sharon Awuor <sawuor395@gmail.com>
2023-06-28 13:39:31 +0300

    completed

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,23 @@
 class Dog
-
-end
+    attr_reader :breed
+  
+    # Setter method for name
+    def name=(name)
+      @name = name
+    end
+  
+    # Getter method for name
+    def name
+      @name
+    end
+  
+    # Setter method for breed
+    def breed=(new_breed)
+      @breed = new_breed
+    end
+  end
+  
+  snoopy = Dog.new
+  snoopy.breed = "Beagle"
+  puts snoopy.breed
+  
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,23 @@
 class Person
-
-end
+    attr_reader :job
+  
+    # Setter method for name
+    def name=(name)
+      @name = name
+    end
+  
+    # Getter method for name
+    def name
+      @name
+    end
+  
+    # Setter method for job
+    def job=(new_job)
+      @job = new_job
+    end
+  end
+  
+  beyonce = Person.new
+  beyonce.job = "Single"
+  puts beyonce.job
+  
